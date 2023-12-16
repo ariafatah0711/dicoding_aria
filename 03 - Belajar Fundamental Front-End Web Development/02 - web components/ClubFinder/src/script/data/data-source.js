99aa3651e9860a9848e93abae112496b90c097ec
ariafatah0711 <ariafatah0711@gmail.com>
2023-12-16 19:22:43 +0700

    update

---
+++ 03 - Belajar Fundamental Front-End Web Development/02 - web components/ClubFinder/src/script/data/data-source.js
@@ -0,0 +1,17 @@
+class DataSource {
+  static searchClub(keyword) {
+    return new Promise((resolve, reject) => {
+      const filteredClubs = clubs.filter((club) =>
+        club.name.toUpperCase().includes(keyword.toUpperCase())
+      );
+
+      if (filteredClubs.length) {
+        resolve(filteredClubs);
+      } else {
+        reject(`${keyword} is not found`);
+      }
+    });
+  }
+}
+
+export default DataSource;
