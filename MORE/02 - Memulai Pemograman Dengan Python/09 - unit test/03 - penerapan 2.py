ae7366e8898853d7dcac6e75448ada8b56acec37
ariafatah0711 <ariafatah0711@gmail.com>
2023-12-18 14:55:04 +0700

    update

--- MORE/02 - Memulai Pemograman Dengan Python/09 - unit test/03 - penerapan 2.py
+++ MORE/02 - Memulai Pemograman Dengan Python/09 - unit test/03 - penerapan 2.py
@@ -18,21 +18,24 @@ class User:
         self.aktif = True
 
 class TestUser(unittest.TestCase):
+    # Test Fixture
+    def setUp(self):
+        self.db = koneksi_ke_db()
+        self.dicoding = User(self.db, "dicoding")
+ 
+    def tearDown(self):
+        putus_koneksi_db(self.db)
+ 
     # Test Case 1
     def test_user_default_not_active(self):
-        db = koneksi_ke_db()
-        dicoding = User(db, "dicoding")
-        self.assertFalse(dicoding.aktif) # tidak aktif secara default
-        putus_koneksi_db(db)
-       
-    # Test Case 2 
-    def test_user_is_active(self):
-        db = koneksi_ke_db()
-        dicoding = User(db, "dicoding")
-        dicoding.set_aktif() # aktifkan user baru
-        self.assertTrue(dicoding.aktif)
-        putus_koneksi_db(db)
+        self.assertFalse(self.dicoding.aktif)  # tidak aktif secara default
         
+    # Test Case 2
+    def test_user_is_active(self):
+        self.dicoding.set_aktif()  # aktifkan user baru
+        self.assertTrue(self.dicoding.aktif)
+
 if __name__ == "__main__":
     # Test Runner
     unittest.main()
+    
