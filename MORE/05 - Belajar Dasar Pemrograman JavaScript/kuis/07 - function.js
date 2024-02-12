4d615a8196b3b9eed322c2ae025a33be28a7f5db
Aria Fatah <ariafatah0711@gmail.com>
2024-02-12 20:35:56 +0700

    update

---
+++ MORE/05 - Belajar Dasar Pemrograman JavaScript/kuis/07 - function.js
@@ -0,0 +1,47 @@
+/**
+ * TODO:
+ * 1. Buatlah fungsi bernama minimal dengan ketentuan berikut:
+ *    - Menerima dua buah argumen number, a dan b.
+ *    - Mengembalikan nilai terkecil antara a atau b.
+ *    - Bila nilai keduanya sama, maka kembalikan dengan nilai a
+ *
+ *    contoh:
+ *    minimal(1, 4) // output: 1
+ *    minimal(3, 2) // output: 2
+ *    minimal(3, 3) // output: 3
+ *
+ * 2. Buatlah sebuah function bernama findIndex yang menerima dua parameter, yaitu array dan number.
+ *    Fungsi tersebut harus mengembalikan index dari angka yang sesuai pada array tersebut.
+ *    Jika angka tidak ditemukan, maka kembalikan nilai -1.
+ *
+ *    contoh:
+ *    findIndex([1, 2, 3, 4, 5], 3) // output: 2
+ *    findIndex([1, 2, 3, 4, 5], 6) // output: -1
+ *    findIndex([1, 2, 3, 4, 5], 5) // output: 4
+ */
+
+// Tulis kode di bawah ini
+
+function minimal(a, b) {
+  if (a <= b) {
+    return a;
+  } else {
+    return b;
+  }
+}
+
+console.log(minimal(1, 4)); // output: 1
+console.log(minimal(3, 2)); // output: 2
+console.log(minimal(3, 3)); // output: 3
+
+function findIndex(array, number) {
+  if (array.includes(number)) {
+    return number - 1;
+  } else {
+    return -1;
+  }
+}
+
+console.log(findIndex([1, 2, 3, 4, 5], 3)); // output: 2
+console.log(findIndex([1, 2, 3, 4, 5], 6)); // output: -1
+console.log(findIndex([1, 2, 3, 4, 5], 5)); // output: 4
