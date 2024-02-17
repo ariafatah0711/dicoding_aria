9c7097b048a564c5f8abd24c101a4422148b65c0
Aria Fatah <ariafatah0711@gmail.com>
2024-02-17 21:44:16 +0700

    update

---
+++ 05 - Menjadi Front-End Web Developer Expert/01 - dicoding-blog/webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    open: true,
+    port: 9000,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: path.resolve(__dirname, 'dist/index.html'),
+      template: path.resolve(__dirname, 'src/index.html'),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/public'),
+          to: path.resolve(__dirname, 'dist/public'),
+        },
+      ],
+    }),
+    new FaviconsWebpackPlugin({
+      logo: path.resolve(__dirname, 'src/public/dicoding.jpeg'),
+
+      outputPath: 'public/favicons',
+
+      publicPath: 'public',
+      prefix: 'favicons/',
+    }),
+  ],
+};
