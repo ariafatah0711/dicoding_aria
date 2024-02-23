efd0360ec2cbb22d08280a85568cb0ef5b0ed58c
Aria Fatah <ariafatah0711@gmail.com>
2024-02-24 05:56:29 +0700

    update

--- 05 - Menjadi Front-End Web Developer Expert/submission/01 - restaurant-apps/src/scripts/view/main.js
+++ 05 - Menjadi Front-End Web Developer Expert/submission/01 - restaurant-apps/src/scripts/view/main.js
@@ -20,24 +20,6 @@ const main = async () => {
   $("main, .nav a").click(() => {
     $("#drawer").removeClass("open");
   });
-
-  // function search() {
-  //   let input, filter, li, h3;
-  //   input = document.getElementById("explore-search");
-  //   filter = input.value.toUpperCase();
-  //   li = document.querySelectorAll("li");
-
-  //   for (let i = 0; i < li.length; i++) {
-  //     h3 = li[i].getElementsByTagName("h3")[0];
-  //     txtValue = h3.textContent || h3.innerText;
-
-  //     if (txtValue.toUpperCase().indexOf(filter) > -1) {
-  //       li[i].style.display = "";
-  //     } else {
-  //       li[i].style.display = "none";
-  //     }
-  //   }
-  // }
 };
 
 export default main;
