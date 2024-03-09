49a5fb093b4ce3df31fec52d4ffaafda5ce36eb0
Aria Fatah <ariafatah0711@gmail.com>
2024-03-09 18:46:43 +0700

    update

--- 05 - Menjadi Front-End Web Developer Expert/04 - Progressive Web Apps/01 - project/src/scripts/globals/config.js
+++ 05 - Menjadi Front-End Web Developer Expert/04 - Progressive Web Apps/01 - project/src/scripts/globals/config.js
@@ -3,6 +3,7 @@ const CONFIG = {
   BASE_URL: "https://api.themoviedb.org/3/",
   BASE_IMAGE_URL: "https://image.tmdb.org/t/p/w500/",
   DEFAULT_LANGUANGE: "en-us",
+  CACHE_NAME: new Date().toISOString(),
 };
 
 export default CONFIG;
--- 05 - Menjadi Front-End Web Developer Expert/04 - Progressive Web Apps/01 - project/src/scripts/sw.js
+++ 05 - Menjadi Front-End Web Developer Expert/04 - Progressive Web Apps/01 - project/src/scripts/sw.js
@@ -1,13 +1,31 @@
+import CacheHelper from "./utils/cache-helper";
+
+// Daftar asset yang akan di-caching
+const assetsToCache = [
+  "./",
+  "./icons/icon-72x72.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png",
+  "./index.html",
+  "./favicon.png",
+  "./app.bundle.js",
+  "./app.webmanifest",
+  "./sw.bundle.js",
+];
+
 self.addEventListener("install", (event) => {
-  console.log("Installing Service Worker!");
+  event.waitUntil(CacheHelper.cachingAppShell([...assetsToCache]));
 });
 
 self.addEventListener("active", (event) => {
-  console.log("Active Service Worker");
+  event.waitUntil(CacheHelper.deleteOldCache());
 });
 
 self.addEventListener("fetch", (event) => {
-  console.log(event.request);
-
-  event.respondWith(fetch(event.request));
+  event.respondWith(CacheHelper.revalidateCache(event.request));
 });
