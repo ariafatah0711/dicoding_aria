14bda685af9e8da51351af5362fa678168568dc0
Aria Fatah <ariafatah0711@gmail.com>
2024-03-11 21:35:25 +0700

    update

--- 05 - Menjadi Front-End Web Developer Expert/04 - Progressive Web Apps/01 - project/src/scripts/globals/config.js
+++ 05 - Menjadi Front-End Web Developer Expert/04 - Progressive Web Apps/01 - project/src/scripts/globals/config.js
@@ -1,11 +1,12 @@
 const CONFIG = {
-  KEY: "15b0aeb8d05ad5451a081cbf368ce8c3",
+  // KEY: "15b0aeb8d05ad5451a081cbf368ce8c3",
   // KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNWIwYWViOGQwNWFkNTQ1MWEwODFjYmYzNjhjZThjMyIsInN1YiI6IjY1ZTk3NjU1N2M2ZGUzMDE2MzA4MGRhNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xeLA5aKUV0M42gq5DO5lnMgD9NBzB3EJcmGLbgLBL4o",
+  KEY: "4a9b557e20b5088a43b6cd0b12a592de",
   BASE_URL: "https://api.themoviedb.org/3/",
   BASE_IMAGE_URL: "https://image.tmdb.org/t/p/w500/",
   DEFAULT_LANGUANGE: "en-us",
-  CACHE_NAME: new Date().toISOString(),
-  // CACHE_NAME: "movie",
+  // CACHE_NAME: new Date().toISOString(),
+  CACHE_NAME: "movie",
 };
 
 export default CONFIG;
---
+++ 05 - Menjadi Front-End Web Developer Expert/04 - Progressive Web Apps/02 - movie-catalogue/src/scripts/globals/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  KEY: "4a9b557e20b5088a43b6cd0b12a592de",
+  BASE_URL: "https://api.themoviedb.org/3/",
+  BASE_IMAGE_URL: "https://image.tmdb.org/t/p/w500/",
+  DEFAULT_LANGUAGE: "en-us",
+  CACHE_NAME: "MovieCatalogue-V1",
+};
+
+export default CONFIG;
