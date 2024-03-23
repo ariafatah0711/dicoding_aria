a4f99e13152bdf32558626c583b785f48a38b21b
Aria Fatah <ariafatah0711@gmail.com>
2024-03-23 13:54:29 +0700

    update

--- 05 - Menjadi Front-End Web Developer Expert/05 - automation testing/02 - movie-catalogue-jest-configured/tests/favoriteMovieSearch.test.js
+++ 05 - Menjadi Front-End Web Developer Expert/05 - automation testing/02 - movie-catalogue-jest-configured/tests/favoriteMovieSearch.test.js
@@ -3,38 +3,99 @@ import FavoriteMovieSearchPresenter from "../src/scripts/views/pages/liked-movie
 import FavoriteMovieIdb from "../src/scripts/data/favorite-movie-idb";
 
 describe("Searching movies", () => {
-  beforeEach(() => {
+  let presenter;
+
+  const searchMovies = (query) => {
+    const queryElement = document.getElementById("query");
+    queryElement.value = query;
+
+    queryElement.dispatchEvent(new Event("change"));
+  };
+
+  const setMovieSearchContainer = () => {
     document.body.innerHTML = `
-          <div id="movie-search-container">
-            <input id="query" type="text">
-            <div class="movie-result-container">
-              <ul class="movies">
-              </ul>
-            </div>
-          </div>
-        `;
+      <div id="movie-search-container">
+        <input id="query" type="text">
+        <div class="movie-result-container">
+          <ul class="movies">
+          </ul>
+        </div>
+      </div>
+    `;
+  };
+
+  const constructPresenter = () => {
+    spyOn(FavoriteMovieIdb, "searchMovies");
+
+    presenter = new FavoriteMovieSearchPresenter({
+      favoriteMovies: FavoriteMovieIdb,
+    });
+  };
+
+  beforeEach(() => {
+    setMovieSearchContainer();
+    constructPresenter();
   });
 
   it("should be able to capture the query typed by the user", () => {
-    spyOn(FavoriteMovieIdb, "searchMovies");
-
-    const presenter = new FavoriteMovieSearchPresenter({ favoriteMovies: FavoriteMovieIdb });
-    const queryElement = document.getElementById("query");
-    queryElement.value = "film a";
-    queryElement.dispatchEvent(new Event("change"));
+    searchMovies("film a");
 
     expect(presenter.latestQuery).toEqual("film a");
   });
 
   it("should ask the model to search for liked movies", () => {
-    spyOn(FavoriteMovieIdb, "searchMovies");
-
-    // eslint-disable-next-line no-unused-vars
-    const presenter = new FavoriteMovieSearchPresenter({ favoriteMovies: FavoriteMovieIdb });
-    const queryElement = document.getElementById("query");
-    queryElement.value = "film a";
-    queryElement.dispatchEvent(new Event("change"));
+    searchMovies("film a");
 
     expect(FavoriteMovieIdb.searchMovies).toHaveBeenCalledWith("film a");
+  });
+
+  it("should show the found movies", () => {
+    presenter._showFoundMovies([{ id: 1 }]);
+    expect(document.querySelectorAll(".movie").length).toEqual(1);
+
+    presenter._showFoundMovies([
+      {
+        id: 1,
+        title: "Satu",
+      },
+      {
+        id: 2,
+        title: "Dua",
+      },
+    ]);
+    expect(document.querySelectorAll(".movie").length).toEqual(2);
+  });
+
+  it("should show the title of the found movies", () => {
+    presenter._showFoundMovies([
+      {
+        id: 1,
+        title: "Satu",
+      },
+    ]);
+
+    expect(document.querySelectorAll(".movie__title").item(0).textContent).toEqual("Satu");
+
+    presenter._showFoundMovies([
+      {
+        id: 1,
+        title: "Satu",
+      },
+      {
+        id: 2,
+        title: "Dua",
+      },
+    ]);
+
+    const movieTitles = document.querySelectorAll(".movie__title");
+
+    expect(movieTitles.item(0).textContent).toEqual("Satu");
+    expect(movieTitles.item(1).textContent).toEqual("Dua");
+  });
+
+  it("should show - for found movie without title", () => {
+    presenter._showFoundMovies([{ id: 1 }]);
+
+    expect(document.querySelectorAll(".movie__title").item(0).textContent).toEqual("-");
   });
 });
