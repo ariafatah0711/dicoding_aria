11ac55f4ea9d100f57f460f4c5f7c4d3b838dc3f
Aria Fatah <ariafatah0711@gmail.com>
2024-05-28 15:51:09 +0700

    update now

---
+++ 04 - Belajar Tools Front-End Web Intermediate/04 - dom lit/02 - money-tracker-app/src/js/components/NavLinkAuth.js
@@ -0,0 +1,33 @@
+import { html, nothing } from 'lit';
+import LitWithoutShadowDom from './base/LitWithoutShadowDom';
+
+class NavLinkAuth extends LitWithoutShadowDom {
+  redner() {
+    return html`
+      <li class="nav-item dropdown">
+        <a
+          class="nav-link dropdown-toggle text-nowrap"
+          href="#"
+          role="button"
+          data-bs-toggle="dropdown"
+        >
+          <div class="me-2 d-inline-block">
+            <img
+              id="imgUserLogged"
+              style="width: 30px;height: 30px"
+              class="img-fluid rounded-pill"
+              src="https://ui-avatars.com/api/?name=User%20Name&background=random"
+              alt="User Name"
+            />
+          </div>
+          <span id="nameUserLogged"></span>
+        </a>
+        <ul class="dropdown-menu">
+          <a class="dropdown-item" id="userLogOut"> Log Out </a>
+        </ul>
+      </li>
+    `;
+  }
+}
+
+customElements.define('nav-link-auth', NavLinkAuth);
