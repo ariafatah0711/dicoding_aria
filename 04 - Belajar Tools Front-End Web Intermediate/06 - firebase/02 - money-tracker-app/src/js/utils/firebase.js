082d9997497116dcd9449d21a14740e9e8c1e859
Aria Fatah <ariafatah0711@gmail.com>
2024-06-05 20:59:15 +0700

    update

---
+++ 04 - Belajar Tools Front-End Web Intermediate/06 - firebase/02 - money-tracker-app/src/js/utils/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCKqnhe-SEmnh9ZQosrgf-MpRH7-f2CRtA',
+  authDomain: 'money-tracker-app-59f88.firebaseapp.com',
+  projectId: 'money-tracker-app-59f88',
+  storageBucket: 'money-tracker-app-59f88.appspot.com',
+  messagingSenderId: '484297306976',
+  appId: '1:484297306976:web:5f6c58da0e0249635d2405',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export { app, auth };
