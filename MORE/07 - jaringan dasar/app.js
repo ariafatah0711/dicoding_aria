2ece591329935ac2995fb9b00a33ba9f776c9d9f
Aria Fatah <ariafatah0711@gmail.com>
2024-11-02 22:55:21 +0700

    update

---
+++ MORE/07 - jaringan dasar/app.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const express = require("express");
+
+const PORT = 8000;
+const HOST = "0.0.0.0";
+
+const app = express();
+/*
+[ROUTE] '/'
+[Response] => Hello world!
+ */
+app.get("/", (req, res) => {
+  res.send("Hello world!\n");
+});
+
+/*
+Optional [ROUTE] '/me'
+[Response] => Dicoding username.
+*/
+app.get("/me", (req, res) => {
+  res.send("ariafatah\n");
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
